283b8a62d1f50337b65901d989e3b87315b01f35
abhishek <abbhishekk27@gmail.com>
2023-02-19 14:19:21 +0530

    build: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>blitz</title>
+        <meta name="description" content="Reimagining chess" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>blitz</h1>
+      </div>
+    </>
+  );
+}
